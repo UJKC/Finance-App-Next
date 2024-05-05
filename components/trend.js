d685b94eaa6d148688b8ef3378446e6dca1573fb
UJKC <ujwalkcsps@gmail.com>
2024-05-05 05:24:41 -0700

    hooks

--- components/trend.js
+++ components/trend.js
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { ArrowDownLeft, ArrowUpRight } from 'lucide-react';
+import { useFormatCurrency } from "@/hooks/use-format-currency";
 
 export default function Trend({type, amount, prevAmount}) {
     const colorClasses = {
@@ -16,12 +17,12 @@ export default function Trend({type, amount, prevAmount}) {
 
     const percentageChange = useMemo(() => calPercentageChange(amount, prevAmount).toFixed(0), [amount, prevAmount])
 
-    const formatCurrency = (amount) => new Intl.NumberFormat('en-US', {style: 'currency', currency: 'EUR'}).format(amount)
+    const formattedAmount = useFormatCurrency(amount)
 
     return <div>
         <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>
         <div className="font-semibold text-black dark:text-white mb-2">
-            {amount ? formatCurrency(amount) : formatCurrency(0)}
+            {formattedAmount}
         </div>
         <div className="flex space-x-1 items-center text-sm">
             {percentageChange <= 0 && <ArrowDownLeft className="text-red-700 dark:text-red-300"/>}
