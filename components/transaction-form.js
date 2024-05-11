a0906fa45803c89e90c40dae7a0432d7ea64a4b6
UJKC <ujwalkcsps@gmail.com>
2024-05-11 18:14:16 +0530

    react hook form

--- components/transaction-form.js
+++ components/transaction-form.js
@@ -1,39 +1,46 @@
+'use client'
+
 import { categories, types } from "@/lib/consts";
 import Label from "./label";
 import Select from "./select";
 import Input from "./input";
 import Button from "./button";
+import { useForm } from "react-hook-form";
 
 export default function TransactionForm() {
-    return <form className="space-y-4">
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
+    return <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
                 <Label className="mb-1">Type</Label>
-                <Select>
+                <Select {...register("type")}>
                     {types.map(type => <option key={type}>{type}</option>)}
                 </Select>
             </div>
 
             <div>
                 <Label className="mb-1">Category</Label>
-                <Select>
+                <Select {...register("category")}>
                     {categories.map(category => <option key={category}>{category}</option>)}
                 </Select>
             </div>
 
             <div>
                 <Label className="mb-1">Date</Label>
-                <Input />
+                <Input {...register("created_at")} />
             </div>
 
             <div>
                 <Label className="mb-1">Amount</Label>
-                <Input type='number' />
+                <Input type='number' {...register("amount")} />
             </div>
 
             <div className="col-span-2">
                 <Label className="mb-1">Description</Label>
-                <Input type='number' />
+                <Input type='number' {...register("description")} />
             </div>
 
             <div className="flex justify-end">
