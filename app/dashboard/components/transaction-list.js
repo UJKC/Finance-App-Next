2741aa721393e570eb0ee415364145254506e160
UJKC <ujwalkcsps@gmail.com>
2024-05-17 14:59:45 +0530

    project close

--- app/dashboard/components/transaction-list.js
+++ app/dashboard/components/transaction-list.js
@@ -6,19 +6,30 @@ import TransactionSummaryItem from "@/components/transaction-summary-item"
 import { fetchTransaction } from "@/lib/actions"
 import { groupAndSumTransactionsByDate } from "@/lib/utils"
 import { useState } from "react"
+import { Loader } from "lucide-react"
 
 
 export default async function TransactionList({ range, initialTransaction }) {
   const [transactions, setTransactions] = useState(initialTransaction)
   const [offset, setoffset] = useState(initialTransaction.length)
+  const [buttonHidden, setButtonHidden] = useState(initialTransaction.length == 0)
+  const [loading, setLoading] = useState(false)
   const grouped = groupAndSumTransactionsByDate(transactions)
   const handleChange = async (e) => {
-    const nextTransaction = await fetchTransaction(range, offset, 10)
-    setoffset(prevValue => prevValue + 10)
-    setTransactions(prevTransactions => [
-      ...prevTransactions,
-      ...nextTransaction
-    ])
+    setLoading(true)
+    let nextTransaction = null
+    try {
+      nextTransaction = await fetchTransaction(range, offset, 10)
+      setoffset(prevValue => prevValue + 10)
+      setButtonHidden(nextTransaction.length == 0)
+      setTransactions(prevTransactions => [
+        ...prevTransactions,
+        ...nextTransaction
+      ])
+    }
+    finally {
+      setLoading(false)
+    }
   }
   return (
     <div className="space-y-8">
@@ -37,9 +48,16 @@ export default async function TransactionList({ range, initialTransaction }) {
           </div>
         )
       }
-      <div className="flex justify-between">
-        <Button variant='ghost' onClick={handleChange}>Load More</Button>
-      </div>
+      {transactions.length == 0 && <div className="text-center text-gray-400 dark:text-gray-500">No mire transactions found</div>}
+      {!buttonHidden && <div className="flex justify-between">
+        <Button variant='ghost' onClick={handleChange} disabled={loading} >
+          <div className="flex items-center space-x-1">
+            {loading && <Loader>
+              <div>Load More</div>
+            </Loader>}
+          </div>
+        </Button>
+      </div>}
     </div>
   )
 }
